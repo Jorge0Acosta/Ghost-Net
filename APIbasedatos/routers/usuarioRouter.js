f767bf828b35b7efd643313bd80cf6eac10e9c78
JayDz7 <riacrardogarcia@gmail.com>
2026-07-09 22:01:33 -0600

    Actualizacion

--- APIbasedatos/routers/usuarioRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const usuarioControladores = require("../controladores/usuarioControladores");
-
-router.post("/registro", usuarioControladores.registrar);
-router.post("/login", usuarioControladores.login);
-
-module.exports = router;
